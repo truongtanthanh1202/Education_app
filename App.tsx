fd0f9c11d3a0964c156272bfd1c310a60b775fc9
Tran Quy Manh - 21020352 <21020352@vnu.edu.vn>
2023-04-16 23:45:09 +0700

    forgot password

--- App.tsx
+++ App.tsx
@@ -6,18 +6,23 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Navbar from './components/screens/nav-bar';
 
 //Screen components
-import { Splash1, Splash2, Splash3, Splash4 } from './components/screens/splash';
+import {Splash1, Splash2, Splash3, Splash4} from './components/screens/splash';
 import Login from './components/screens/login';
-import { Register1, Register2, Register3 } from './components/screens/register';
-import { Forgot1, Forgot2, Forgot3, Forgot4 } from './components/screens/forgot-password';
-import { Profile3, Profile4, Profile5 } from './components/screens/profile';
+import {Register1, Register2, Register3} from './components/screens/register';
+import {
+  Forgot1,
+  Forgot2,
+  Forgot3,
+  Forgot4,
+} from './components/screens/forgot-password';
+import {Profile3, Profile4, Profile5} from './components/screens/profile';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-     <NavigationContainer>
+    <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        {/* <Stack.Screen name="Splash1" component={Splash1} />
+        <Stack.Screen name="Splash1" component={Splash1} />
         <Stack.Screen name="Splash2" component={Splash2} />
         <Stack.Screen name="Splash3" component={Splash3} />
         <Stack.Screen name="Splash4" component={Splash4} />
@@ -28,10 +33,10 @@ const App = () => {
         <Stack.Screen name="Forgot1" component={Forgot1} />
         <Stack.Screen name="Forgot2" component={Forgot2} />
         <Stack.Screen name="Forgot3" component={Forgot3} />
-        <Stack.Screen name="Forgot4" component={Forgot4} /> */}
+        <Stack.Screen name="Forgot4" component={Forgot4} />
         <Stack.Screen name="Profile5" component={Profile5} />
 
-        {/* <Stack.Screen name="Navbar" component={Navbar} /> */}
+        <Stack.Screen name="Navbar" component={Navbar} />
       </Stack.Navigator>
     </NavigationContainer>
   );
