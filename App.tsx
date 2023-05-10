0ccd67fb26c50c04705784e3bf471eef5d5a87d1
Hakuryo0413 <100341996+Hakuryo0413@users.noreply.github.com>
2023-05-11 00:57:03 +0700

    Add FirstName, LastName,.... in Register 2

--- App.tsx
+++ App.tsx
@@ -35,7 +35,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        {/* <Stack.Screen name="Splash1" component={Splash1} />
+        <Stack.Screen name="Splash1" component={Splash1} />
         <Stack.Screen name="Splash2" component={Splash2} />
         <Stack.Screen name="Splash3" component={Splash3} />
         <Stack.Screen name="Splash4" component={Splash4} />
@@ -46,7 +46,7 @@ const App = () => {
         <Stack.Screen name="Forgot1" component={Forgot1} />
         <Stack.Screen name="Forgot2" component={Forgot2} />
         <Stack.Screen name="Forgot3" component={Forgot3} />
-        <Stack.Screen name="Forgot4" component={Forgot4} /> */}
+        <Stack.Screen name="Forgot4" component={Forgot4} />
         <Stack.Screen name="Navbar" component={Navbar} />
         <Stack.Screen name="Search2" component={Search2} />
         <Stack.Screen name="Course" component={Course} />
